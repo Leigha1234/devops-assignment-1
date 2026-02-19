48c496d890a04df4c042f64f8fc771dcd27daec2
Leigha1234 <leigha0305@gmail.com>
2026-02-19 17:56:59 +0000

    Trigger the build.

--- app.py
+++ app.py
@@ -55,6 +55,5 @@ def index():
         except ValueError:
             result = "Invalid input"
     return render_template('index.html', result=result)
-
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
